d0230fbdeddf2a81cb273cde68e9b0fcc3d7c72b
sake12 <sake12@gmail.com>
2016-03-26 15:59:59 +0100

    Second test.

--- learntris.py
+++ learntris.py
@@ -1,2 +1,17 @@
-# this is learntris.py
-pass
+from src.board import *
+
+
+def main():
+    """ Main function """
+    board = Board()
+    while True:
+        code = input()
+        if code == "q":
+            exit()
+        elif code == "p":
+            board.print_board()
+        else:
+            print("Wrong code!")
+
+if __name__ == '__main__':
+    main()
